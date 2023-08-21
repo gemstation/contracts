becc2a38966ef163e4ce34645cb684c99c5a03cf
Ramesh Nair <ram@hiddentao.com>
2023-08-21 09:14:55 +0100

    feat: verify on every chain

--- scripts/verify.js
+++ scripts/verify.js
@@ -6,23 +6,28 @@ let $
 
   const addresses = require('../gemforge.deployments.json')
 
-  const chainId = '11155111'
-
-  console.log(`Verifying on chain ${chainId} ...`)
-
-  const contracts = addresses[chainId] || []
-
-  for (let { name, contract } of contracts) {
-    let args = '0x'
-
-    if (contract.constructorArgs.length) {
-      args = (await $`cast abi-encode constructor(address) ${contract.constructorArgs.join(' ')}`).stdout
+  for (let chainId in addresses) {
+    // skip localhost
+    if (chainId === '31337') {
+      continue
     }
 
-    console.log(`Verifying ${name} at ${contract.address} with args ${args}`)
+    console.log(`Verifying on chain ${chainId} ...`)
 
-    await $`forge verify-contract ${contract.address} ${name} --constructor-args ${args} --chain-id ${chainId} --verifier etherscan --etherscan-api-key ${process.env.ETHERSCAN_API_KEY} --watch`
+    const contracts = addresses[chainId] || []
 
-    console.log(`Verified!`)
+    for (let { name, contract } of contracts) {
+      let args = '0x'
+
+      if (contract.constructorArgs.length) {
+        args = (await $`cast abi-encode constructor(address) ${contract.constructorArgs.join(' ')}`).stdout
+      }
+
+      console.log(`Verifying ${name} at ${contract.address} with args ${args}`)
+
+      await $`forge verify-contract ${contract.address} ${name} --constructor-args ${args} --chain-id ${chainId} --verifier etherscan --etherscan-api-key ${process.env.ETHERSCAN_API_KEY} --watch`
+
+      console.log(`Verified!`)
+    }
   }
 })()
